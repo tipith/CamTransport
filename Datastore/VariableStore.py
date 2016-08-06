c42389bc1f8c070b73aaea16dcb1054c891eb6bf
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-08-06 23:09:33 +0300

    More python2 compatibility issues

--- Datastore/VariableStore.py
+++ Datastore/VariableStore.py
@@ -4,15 +4,16 @@ import cam_config
 
 
 def set_variable(src, name, value):
-    file = 'vars.json'
+    var_file = os.path.join(cam_config.variable_path, 'vars.json')
     data = {}
 
     try:
-        with open(os.path.join(file), 'r') as f:
+        with open(var_file, 'r') as f:
             data = json.loads(f.read())
-    except (IOError, json.decoder.JSONDecodeError):
-        data = {}
-        pass
+    except IOError:
+        print('no variables file')
+    except ValueError:
+        print('invalid data file')
 
     data.setdefault(src, {})
     data[src][name] = value
@@ -21,5 +22,5 @@ def set_variable(src, name, value):
         print("set_variable: creating path", cam_config.variable_path)
         os.makedirs(cam_config.variable_path)
 
-    with open(os.path.join(cam_config.variable_path, file), 'w') as f:
+    with open(var_file, 'w') as f:
         json.dump(data, f)
