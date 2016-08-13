e400047d84b80c0c76bddd88ef5ad4bd9c9b64d7
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-08-13 12:43:58 +0300

    add last heard time

--- Datastore/VariableStore.py
+++ Datastore/VariableStore.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import cam_config
 
 
@@ -17,6 +18,7 @@ def set_variable(src, name, value):
 
     data.setdefault(src, {})
     data[src][name] = value
+    data[src]["last_heard"] = time.strftime("%Y-%m-%d %H:%M:%S")
 
     if not os.path.exists(cam_config.variable_path):
         print("set_variable: creating path", cam_config.variable_path)
