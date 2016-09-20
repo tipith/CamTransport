2a362f51f02557eb19d7b1550f1fd9d2007389ce
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-09-20 20:48:49 +0300

    More refactoring

--- script_cmd.py
+++ script_cmd.py
@@ -8,8 +8,7 @@ cmd_logger = logging.getLogger('CmdSender')
 
 
 if __name__ == "__main__":
-    cmd_logger.info('start sending command %s: %s' % (sys.argv[1], sys.argv[2]))
+    cmd_logger.info('command %s: %s' % (sys.argv[1], sys.argv[2]))
     local_messaging = Messaging.LocalClientMessaging()
     local_messaging.send(Messaging.Message.msg_command(sys.argv[1], sys.argv[2]))
     local_messaging.stop()
-    cmd_logger.info('stop')
--- script_img.py
+++ script_img.py
@@ -8,8 +8,7 @@ img_logger = logging.getLogger('ImgSender')
 
 
 if __name__ == "__main__":
-    img_logger.info('start sending %s' % sys.argv[1])
+    img_logger.info('image %s' % sys.argv[1])
     local_messaging = Messaging.LocalClientMessaging()
     local_messaging.send(Messaging.Message.msg_image(sys.argv[1]))
     local_messaging.stop()
-    img_logger.info('stop sending')
