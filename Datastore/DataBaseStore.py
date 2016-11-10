1f63010b0681d81690f190aa3fde9786b1a30a34
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-11-11 01:06:13 +0200

    store light and movement events to database

--- Datastore/DataBaseStore.py
+++ Datastore/DataBaseStore.py
@@ -1,7 +1,6 @@
 import datetime
 import logging
 import MySQLdb
-import re
 
 import cam_config
 
@@ -23,19 +22,34 @@ def db_store_image(cam_id, timestamp, location, size):
     upload_time = (datetime.datetime.now() - timestamp).total_seconds()
     db = open_db()
     cur = db.cursor()
-
     try:
         cur.execute("INSERT INTO Picture (Timestamp, idCamera, FileLocation, FileSize, UploadTime) \
                     VALUES (%s, %s, %s, %s, %s)", (timestamp_str, cam_id, location, size, upload_time))
     except MySQLdb.IntegrityError:
         db_logger.warn('unable to add entry')
-
     close_db(db)
 
 
-def db_store_movement(cam_id, timetamp, event):
-    pass
+def db_store_movement(cam_id, timestamp, event):
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    db = open_db()
+    cur = db.cursor()
+    try:
+        cur.execute("INSERT INTO Movement (idCamera, Timestamp, Event) \
+                    VALUES (%s, %s, %s)", (cam_id, timestamp_str, event))
+    except MySQLdb.IntegrityError:
+        db_logger.warn('unable to add entry')
+    close_db(db)
 
 
-def db_store_light_control(cam_id, timetamp, event):
-    pass
+def db_store_light_control(cam_id, timestamp, event):
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    db = open_db()
+    cur = db.cursor()
+    try:
+        cur.execute("INSERT INTO LightControl (idCamera, Timestamp, Event) \
+                    VALUES (%s, %s, %s)", (cam_id, timestamp_str, event))
+    except MySQLdb.IntegrityError:
+        db_logger.warn('unable to add entry')
+    close_db(db)
+
