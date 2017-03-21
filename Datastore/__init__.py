b460153c30547657e109ef8fa8d330fea9391453
Timo Pihlström <timo.pihlstrom@iki.fi>
2017-03-22 00:21:11 +0200

    rpi temperature monitoring

--- Datastore/__init__.py
+++ Datastore/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['add_image', 'add_image_movement', 'set_variable', 'db_store_image', 'db_store_movement', 'db_store_light_control']
+__all__ = ['add_image', 'add_image_movement', 'set_variable', 'db_store_image', 'db_store_movement', 'db_store_light_control', 'db_store_temperature']
 
 # deprecated to keep older scripts who import this from breaking
 from Datastore.DataStore import add_image, add_image_movement, set_variable
-from Datastore.DataBaseStore import db_store_image, db_store_image_movement, db_store_movement, db_store_light_control
+from Datastore.DataBaseStore import db_store_image, db_store_image_movement, db_store_movement, db_store_light_control, db_store_temperature
