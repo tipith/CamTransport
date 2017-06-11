5d026a956106e624faad3f9113856a8db035d00d
Timo Pihlström <timo.pihlstrom@iki.fi>
2017-06-11 21:43:17 +0300

    summer time madness

--- Messaging/__init__.py
+++ Messaging/__init__.py
@@ -4,4 +4,4 @@ __all__ = ['BaseMessaging', 'ClientMessaging', 'ServerMessaging', 'Message', 'Al
 
 # deprecated to keep older scripts who import this from breaking
 from . Messaging import BaseMessaging, ClientMessaging, ServerMessaging, LocalServerMessaging, LocalClientMessaging
-from . Message import Message, AlhoMessageException
+from . Message import *
