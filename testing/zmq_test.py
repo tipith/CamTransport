924f9dc638a3bdb8c688fb5ea7ad80f6f16961c9
Timo Pihlström <timo.pihlstrom@iki.fi>
2017-10-01 17:49:20 +0300

    Add authentication layer to wan messaging

--- testing/zmq_test.py
+++ testing/zmq_test.py
@@ -2,8 +2,8 @@
 # pip install pyzmq
 #
 
+import time
 import glob
-import sys
 import logging
 import queue
 import threading
@@ -59,6 +59,9 @@ if __name__ == "__main__":
 
     pics = glob.glob('test_data/*.png')
 
+    # with curve encryption, it takes some time to connect and negotiate session key
+    time.sleep(0.5)
+
     try:
         for i, pic in enumerate(pics):
             cam_wan.send(Messaging.ImageMessagePeriodical(pic))
@@ -67,26 +70,32 @@ if __name__ == "__main__":
         backend_wan.send(Messaging.CommandMessage('lights', 'on'))
         backend_wan.send(Messaging.CommandMessage('lights', 'off'))
 
+        test_cases = [
+            {
+                'input_queue': client_q,
+                'expected_ids': [Messaging.Message.Command, Messaging.Message.Command]
+            },
+            {
+                'input_queue': server_q,
+                'expected_ids': [Messaging.Message.Image, Messaging.Message.Variable] * len(pics)
+            }
+        ]
+
         test_status = 'SUCCESS'
 
-        # verify client
-        expected_ids = [Messaging.Message.Command, Messaging.Message.Command]
-        for expected_id in expected_ids:
-            received_item = client_q.get(timeout=1.0)
-            if received_item is None or received_item['id'] != expected_id:
-                test_status = 'FAILED'
-                break
-
-        # verify server
-        if test_status == 'SUCCESS':
-            expected_ids = [Messaging.Message.Image, Messaging.Message.Variable] * len(pics)
-            for expected_id in expected_ids:
-                received_item = server_q.get(timeout=1.0)
+        for test_case in test_cases:
+            for expected_id in test_case['expected_ids']:
+                try:
+                    received_item = test_case['input_queue'].get(timeout=1.0)
+                except queue.Empty:
+                    test_status = 'FAILED'
+                    break
                 if received_item is None or received_item['id'] != expected_id:
                     test_status = 'FAILED'
                     break
     finally:
         cam_wan.stop()
         backend_wan.stop()
+        print(test_status)
 
-    print(test_status)
+
